22d5a1000c12b33f0c1debb17ce2368cf0ecb4bf
benyoss <benyoss4@gmail.com>
2020-09-23 16:22:59 -0500

    (create) eslintignore for ignoring linters in bundle

---
+++ client/dist/src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h2>I work too you know!</h2>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
