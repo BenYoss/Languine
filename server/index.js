9b5de57520930b85d90aa4cc4886a155fbce63d8
benyoss <benyoss4@gmail.com>
2020-09-23 16:32:32 -0500

    (create) html display on homepage route using express

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT;
+const { PORT } = process.env;
+
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(PORT, (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    console.log('App listening on ' + PORT + '!');
-})
+  console.log(`App listening on ${PORT}!`);
+});
