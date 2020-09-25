017828caa9b0a50e570683a07af66234f187e82e
benyoss <benyoss4@gmail.com>
2020-09-25 18:51:11 -0500

    (create) routes directory for dbroutes and apiroutes

---
+++ routes/dbroutes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { user } = require('../database/models/models');
+
+const dbrouter = express.Router();
+
+dbrouter.get('/users', (req, res) => {
+  res.send('this is the getter for users');
+});
+
+dbrouter.post('/users', (req, res) => {
+  user.addUser(req.body)
+    .then(() => {
+      console.log('user successfully added!');
+    })
+    .catch((err) => console.error(err));
+
+  res.send('this is the getter for users');
+});
+
+dbrouter.get('/rooms', (req, res) => {
+  res.send('this is the getter for rooms');
+});
+
+dbrouter.get('/languages', (req, res) => {
+  res.send('this is the getter for languages');
+});
+
+dbrouter.get('/messages', (req, res) => {
+  res.send('this is the getter for messages');
+});
+
+dbrouter.get('/files', (req, res) => {
+  res.send('this is the getter for files');
+});
+
+module.exports = {
+  dbrouter,
+};
