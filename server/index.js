bbbc7c9405733d157c916f3e711f870dc294d7e0
benyoss <benyoss4@gmail.com>
2020-09-25 18:49:50 -0500

    (add) dbroutes to server index

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
+const { dbrouter } = require('../routes/dbroutes');
+
 const app = express();
 const { PORT } = process.env;
+app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.use('/', dbrouter);
 
 app.listen(PORT, (err) => {
   if (err) {
