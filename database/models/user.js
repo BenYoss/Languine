e6662ce3b8c64b1544a78a0b19447e1623b0ce7a
benyoss <benyoss4@gmail.com>
2020-09-25 18:50:45 -0500

    (create) models folder with files of each table respectively

---
+++ database/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+require('../index');
+
+const userSchema = new mongoose.Schema({
+  id_google: String,
+  username: String,
+  thumbnail: String,
+  description: String,
+  id_language: Number,
+});
+
+const User = mongoose.model('User', userSchema);
+
+const addUser = ({
+  username, thumbnail, description, idGoogle,
+}) => {
+  const user = new User({
+    username,
+    thumbnail,
+    description,
+    id_google: idGoogle,
+  });
+
+  return User.findOne({ id_google: idGoogle })
+    .then((data) => {
+      if (!data) {
+        return user.save();
+      }
+      return 'DUPE HAS BEEN DETECTED';
+    })
+    .catch((err) => console.error(err));
+};
+
+module.exports = {
+  addUser,
+};
