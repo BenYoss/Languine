fba3cd37bba22a38ea3beaca75e174b49d70f211
benyoss <benyoss4@gmail.com>
2020-09-27 17:09:32 -0500

    (create) googleStrategy file for authentication via google.

---
+++ auth/googleStrategy.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// dotEnv vars
+const cliID = process.env.CLIENT_ID;
+const secret = process.env.CLIENT_SECRET;
+
+// Google strategy for initializing authenticator
+// Use this for obtaining data from user profiles.
+passport.use(new GoogleStrategy({
+  clientID: cliID,
+  clientSecret: secret,
+  callbackURL: 'http://localhost:8080/auth/google/callback',
+},
+((req, accessToken, refreshToken, profile, done) => {
+  const { displayName, id } = profile;
+  const thumbnail = profile.photos[0].value;
+  // Testing data transfer, will correct later.
+  console.log(thumbnail, displayName, id);
+  return done();
+})));
