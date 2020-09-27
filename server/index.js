8c3011753128c9effecc403faa358d98dc093bac
benyoss <benyoss4@gmail.com>
2020-09-27 17:08:17 -0500

    (require) auth/api routers for server use.

--- server/index.js
+++ server/index.js
@@ -3,19 +3,29 @@ const express = require('express');
 const path = require('path');
 
 const { dbrouter } = require('../routes/dbroutes');
+const { authRouter } = require('../routes/authroutes');
+const { apiRouter } = require('../routes/apiroutes');
+
+//----------------------------------------------------------------------------------
 
 const app = express();
 const { PORT } = process.env;
+// Body parser V
 app.use(express.json());
 
+// static files and route utilities
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 app.use('/', dbrouter);
+app.use('/', authRouter);
+app.use('/api', apiRouter);
 
+// app listener for env PORT
 app.listen(PORT, (err) => {
   if (err) {
     console.error(err);
     return;
   }
 
+  // checking if server is listening
   console.log(`App listening on ${PORT}!`);
 });
