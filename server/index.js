43f23ecd65269ed78c6b83d35c70565f2d08e5ba
benyoss <benyoss4@gmail.com>
2020-09-29 10:10:19 -0500

    (insert) passport initialization & (create) servable client-side route configuration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
 
 const { dbrouter } = require('../routes/dbroutes');
 const { authRouter } = require('../routes/authroutes');
@@ -13,11 +14,21 @@ const { PORT } = process.env;
 // Body parser V
 app.use(express.json());
 
+// initialize passport for auth configuration
+app.use(passport.initialize());
+
+// to serve encoded bundle
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 // static files and route utilities
-app.use('/', express.static(path.join(__dirname, '../client/dist')));
 app.use('/', dbrouter);
 app.use('/', authRouter);
 app.use('/api', apiRouter);
+
+// route to serve static html and client-side routes
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 // app listener for env PORT
 app.listen(PORT, (err) => {
