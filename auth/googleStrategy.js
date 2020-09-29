d78d89746d3ce76b28b56f3452abe843ea73c474
benyoss <benyoss4@gmail.com>
2020-09-29 10:06:30 -0500

    (fix) login failure with serialize/deserialize blocks.

--- auth/googleStrategy.js
+++ auth/googleStrategy.js
@@ -6,6 +6,15 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const cliID = process.env.CLIENT_ID;
 const secret = process.env.CLIENT_SECRET;
 
+passport.serializeUser((user, done) => {
+  console.log(user.id, "This user id!");
+  done(null, user);
+});
+
+passport.deserializeUser((idUser, done) => {
+  done(null, idUser);
+});
+
 // Google strategy for initializing authenticator
 // Use this for obtaining data from user profiles.
 passport.use(new GoogleStrategy({
@@ -13,10 +22,10 @@ passport.use(new GoogleStrategy({
   clientSecret: secret,
   callbackURL: 'http://localhost:8080/auth/google/callback',
 },
-((req, accessToken, refreshToken, profile, done) => {
+(req, accessToken, refreshToken, profile, done) => {
   const { displayName, id } = profile;
   const thumbnail = profile.photos[0].value;
   // Testing data transfer, will correct later.
   console.log(thumbnail, displayName, id);
-  return done();
-})));
+  done(null, profile);
+}));
