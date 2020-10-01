4cd0b6242bf7cb4dcb90f3e79e13733960c1b39a
benyoss <benyoss4@gmail.com>
2020-09-30 22:57:12 -0500

    (insert) useEffect to retrieve user information

--- client/src/components/join.jsx
+++ client/src/components/join.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import getUser from '../helpers/helpers';
 
 function Join() {
-  const [name, setName] = useState('');
+  const [user, setUser] = useState('');
   const [room, setRoom] = useState('');
+
+  useEffect(() => {
+    getUser()
+      .then((userInfo) => {
+        setUser(userInfo);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div className="join-outer-container">
       <div className="join-inner-container">
         <h1 className="join-header">Join</h1>
         <div>
-          <input placeholder="" className="User-Input" type="text" onChange={(ev) => setName(ev.target.value)} />
+          <input placeholder={user.username} className="User-Input" type="text" onChange={(ev) => setUser(ev.target.value)} />
           <input placeholder="" className="Room-Input" type="text" onChange={(ev) => setRoom(ev.target.value)} />
-          <Link to={`/discussion?name=${name}&room=${room}`}>
+          <Link to={`/discussion?name=${user.id_google}&room=${room}`}>
             <button className="button" type="submit">Sign In</button>
           </Link>
         </div>
