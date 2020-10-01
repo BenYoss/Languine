3430b7d4a6e037b0d9f8df2a61266702ea198ec7
benyoss <benyoss4@gmail.com>
2020-09-30 22:55:04 -0500

    (create) functional user-to-user messaging service

--- client/src/components/chat.jsx
+++ client/src/components/chat.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import query from 'query-string';
 import io from 'socket.io-client';
+import InfoBar from './infoBar';
 
 let socket;
 
 function Chat() {
-  const [nameo, setName] = useState('');
-  const [roomo, setRoom] = useState('');
+  const [username, setName] = useState('');
+  const [roomType, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const { name, room } = query.parse(window.location.search);
@@ -16,21 +19,52 @@ function Chat() {
     setName(name);
     setRoom(room);
     console.log(query.parse(window.location.search));
-    socket.emit('join', { name, room }, ({ err }) => {
-      alert(err);
+    socket.emit('join', { name, room }, () => {
+
     });
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    };
   }, ['localhost:8080', window.location.search]);
+
+  useEffect(() => {
+    socket.on('message', (msg) => {
+      setMessages([...messages, msg]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  };
+
+  console.log(message, messages);
 
   return (
     <div>
       <h1>This will be the chat</h1>
       <h2>
-        {nameo}
+        {username}
         {' '}
-        and
+        in Room:
         {' '}
-        {roomo}
+        {roomType}
       </h2>
+
+      <div>
+        <InfoBar room={roomType} />
+        <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) => (event.key === 'Enter' ? sendMessage(event) : null)}
+        />
+      </div>
     </div>
   );
 }
