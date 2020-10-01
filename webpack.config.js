987f9bf54009ad375ea313574b13839c17fd9d12
benyoss <benyoss4@gmail.com>
2020-10-01 11:01:46 -0500

    (insert) css config for css compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   devtool: 'cheap-module-eval-source-map',
