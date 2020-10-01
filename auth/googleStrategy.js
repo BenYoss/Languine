11147b66a1f11ba5299373ad048475e34f4aff5d
benyoss <benyoss4@gmail.com>
2020-09-30 22:52:44 -0500

    (insert) addUser to add user in database directly

--- auth/googleStrategy.js
+++ auth/googleStrategy.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const userModel = require('../database/models/models').User;
+//------------------------------------------------------------------
 
 // dotEnv vars
 const cliID = process.env.CLIENT_ID;
 const secret = process.env.CLIENT_SECRET;
 
+// for recording the user information
 passport.serializeUser((user, done) => {
-  console.log(user.id, "This user id!");
+  // console.log(user.id, 'This user id!');
   done(null, user);
 });
 
+// for recording userID
 passport.deserializeUser((idUser, done) => {
   done(null, idUser);
 });
@@ -23,9 +27,17 @@ passport.use(new GoogleStrategy({
   callbackURL: 'http://localhost:8080/auth/google/callback',
 },
 (req, accessToken, refreshToken, profile, done) => {
-  const { displayName, id } = profile;
-  const thumbnail = profile.photos[0].value;
-  // Testing data transfer, will correct later.
-  console.log(thumbnail, displayName, id);
+  // variable for notation convenience
+  const notation = '_json';
+  // destructuring properties in profile
+  const {
+    name, sub, picture, locale,
+  } = profile[notation];
+  // Profile data then gets saved into database
+  userModel.addUser(name, picture, 'fill me', sub, locale)
+    .then(() => {
+      console.log(`${name}has been added!`);
+    })
+    .catch((err) => { console.error(err); });
   done(null, profile);
 }));
