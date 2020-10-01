84ab4d328e27f4f79d753ef7c31380a3970fb717
benyoss <benyoss4@gmail.com>
2020-09-30 22:59:26 -0500

    (fix) getRooms input parameters, addRoom destructuring, id removal from schema

--- database/models/room.js
+++ database/models/room.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 // Schema for room model to abide by
 const roomSchema = new mongoose.Schema({
-  id: Number,
   name: String,
   description: String,
   is_public: Boolean,
@@ -20,21 +19,21 @@ const Room = mongoose.model('Room', roomSchema);
 // ADD ROOM:
 // takes in two strings and a boolean value and records a new room instance in
 // the database based on the room id being considered a unique instance.
-const addRoom = ({ name, description, isPublic }) => {
+const addRoom = (name, description, isPublic) => {
   const room = new Room({ name, description, isPublic });
-  return Room.findOne({ id: room.id })
+  return Room.findOne({ name: room.name })
     .then((match) => {
       if (!match) {
         return room.save();
       }
-      return 'DUPE FOUND';
+      return match;
     })
     .catch((err) => console.error(err));
 };
 
 // GET ROOMS:
 // when invoked will return all room instances recorded in the database in a list.
-const getRooms = () => Room.find().sort().exec();
+const getRooms = (options) => Room.find(options).sort().exec();
 
 module.exports = {
   addRoom,
