cb311de1a3d395474370e8ad13e2fa88948caae3
benyoss <benyoss4@gmail.com>
2020-10-02 13:40:35 -0500

    (remove) navBar html block from component

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -8,32 +8,13 @@ function Homepage() {
     getUser()
       .then((userInfo) => {
         setUser(userInfo);
+        window.language = userInfo.language;
       });
   }, []);
 
   return (
     <div>
       <h1>This will be the homepage</h1>
-      <a className="login-google-btn" href="/login">
-        <h1>Login with google?</h1>
-      </a>
-      {
-              user
-                ? (
-                  <div>
-                    <h2>
-                      {user.username || 'Log in first'}
-                      :
-                      <img src={user.thumbnail} alt="" />
-                    </h2>
-                    <a className="join-btn" href="/join">
-                      <h1>Join a room</h1>
-                    </a>
-                  </div>
-
-                )
-                : <h1>Nope</h1>
-        }
     </div>
   );
 }
