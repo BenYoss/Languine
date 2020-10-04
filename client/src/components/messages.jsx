8e9b9eda0ef048c9ef973bf3c2fa0e8bd9ce83aa
benyoss <benyoss4@gmail.com>
2020-10-04 14:10:20 -0500

    (fix) scroll error

--- client/src/components/messages.jsx
+++ client/src/components/messages.jsx
@@ -4,15 +4,16 @@ import '../styles/messages.css';
 import Message from './message';
 
 function Messages({ messages, name }) {
+  console.log(name, messages);
   return (
     <div>
-      <ScrollToBottom>
-        {messages.map((message, i) => (
-          <div>
-            <Message message={message} name={name} />
-          </div>
-        ))}
-      </ScrollToBottom>
+      {/* <ScrollToBottom> */}
+      {messages.map((message, i) => (
+        <div>
+          <Message message={message} name={name} />
+        </div>
+      ))}
+      {/* </ScrollToBottom> */}
     </div>
   );
 }
