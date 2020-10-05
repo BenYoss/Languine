d2b9d39df9151a916a3c3dc7639d5b5b9f025b60
benyoss <benyoss4@gmail.com>
2020-10-05 11:25:49 -0500

    (create) getRoom, deleteRoom, getMessages

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -11,10 +11,34 @@ export const getUser = () => new Promise((res, rej) => {
     });
 });
 
+export const getRoom = (roomname) => new Promise((res, rej) => {
+  axios.get('/rooms', { params: { name: roomname } })
+    .then((roomData) => {
+      res(roomData.data);
+    })
+    .catch((err) => rej(err));
+});
+
 export const getRooms = () => new Promise((res, rej) => {
   axios.get('/rooms')
     .then((roomData) => {
       res(roomData.data);
     })
-    .catch((err) => rej);
+    .catch((err) => rej(err));
+});
+
+export const deleteRoom = () => new Promise((res, rej) => {
+  axios.delete('/rooms')
+    .then(() => {
+      res('deleted');
+    })
+    .catch((err) => rej(err));
+});
+
+export const getMessages = (room) => new Promise((res, rej) => {
+  axios.get('/messages', { params: { id_room: room } })
+    .then((messages) => {
+      res(messages.data);
+    })
+    .catch((err) => rej(err));
 });
