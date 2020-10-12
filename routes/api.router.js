5b971f60a24c1ed16bd636aaf0379f555156d46c
benyoss <benyoss4@gmail.com>
2020-10-12 14:18:48 -0500

    (Create) GET and POST for uploaded files

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const pdf = require('pdfjs-dist/es5/build/pdf');
+const { File } = require('../database/models/models');
 
 const apiRouter = express.Router();
+pdf.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker';
+
+apiRouter.get('/uploads', (req, res) => {
+  File.getFiles(req.query)
+    .then((files) => {
+      res.send(files);
+    })
+    .catch((err) => console.error(err));
+});
+
+apiRouter.post('/uploads', (req, res) => {
+  // Imports the Google Cloud client library.
+  const { file, user } = req;
+  File.addFile(user.id, file.buffer, file.originalname, file.size)
+    .then((uploadedFile) => {
+      console.log(uploadedFile, 'was sent');
+      res.end();
+    })
+    .catch((err) => console.error(err));
+});
 
 module.exports = {
   apiRouter,
