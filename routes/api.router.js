321a27fc735cdad9af838eb3e82d3daf09d52ff9
benyoss <benyoss4@gmail.com>
2020-10-19 13:14:23 -0500

    (update) uploads POST to utilize gclouds bucket

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pdf = require('pdfjs-dist/es5/build/pdf');
 const { File } = require('../database/models/models');
+const { uploadFile } = require('../config/Gcloud_Storage');
 
 const apiRouter = express.Router();
 pdf.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker';
@@ -8,20 +9,25 @@ pdf.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker';
 apiRouter.get('/uploads', (req, res) => {
   File.getFiles(req.query)
     .then((files) => {
-      res.send(files);
+      res.status(200).send(files);
     })
-    .catch((err) => console.error(err));
+    .catch(() => res.status(500).end());
 });
 
-apiRouter.post('/uploads', (req, res) => {
-  // Imports the Google Cloud client library.
+apiRouter.post('/bucket', (req, res) => {
   const { file, user } = req;
-  File.addFile(user.id, file.buffer, file.originalname, file.size)
-    .then((uploadedFile) => {
-      console.log(uploadedFile, 'was sent');
-      res.end();
+  uploadFile(file)
+    .then((result) => {
+      if (result.includes('.pdf')) {
+        File.addFile(user.id, result, file.originalname, file.size)
+          .then(() => {
+            res.status(201).end();
+          })
+          .catch((err) => console.error(err));
+      }
+      res.status(201).send(result);
     })
-    .catch((err) => console.error(err));
+    .catch(() => res.status(500).end());
 });
 
 module.exports = {
