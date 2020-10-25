c2143e01e5218fe995587e17a05e42e7e80f290c
benyoss <benyoss4@gmail.com>
2020-10-24 22:45:22 -0500

    (Insert) Footer

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { getAccount, getUsers } from '../helpers/helpers';
 import Chat from './messages/chat';
 import Homepage from './homepage';
+import Footer from './footer';
 import Join from './join';
 import RoomList from './rooms';
 import NavBar from './navBar';
@@ -30,34 +31,45 @@ function App() {
   return (
     <div>
       <NavBar user={user} />
-      <Router>
-        <Route exact path="/">
-          <Homepage user={user} />
-        </Route>
-        <Route path="/discussion">
-          <Chat />
-        </Route>
-        <Route path="/fileupload">
-          <Bucket />
-        </Route>
-        <Route path="/join">
-          <Join />
-        </Route>
-        <Route path="/roomlist">
-          {users.length ? (
-            <RoomList users={users} />
-          ) : ''}
-        </Route>
-        <Route path="/file">
-          <File />
-        </Route>
-        <Route path="/user">
-          <UserProfiles />
-        </Route>
-        <Route path="/profile">
-          <Profile user={user} />
-        </Route>
-      </Router>
+      <div style={{ paddingTop: '50px' }}>
+        <Router>
+          <Route exact path="/">
+            <Homepage user={user} />
+          </Route>
+          <Route path="/discussion">
+            <Chat />
+          </Route>
+          <Route path="/fileupload">
+            <Bucket />
+          </Route>
+          <Route path="/join">
+            <Join />
+          </Route>
+          <Route path="/roomlist">
+            {users.length ? (
+              <RoomList users={users} />
+            ) : ''}
+          </Route>
+          <Route path="/file">
+            <File />
+          </Route>
+          <Route path="/user">
+            <UserProfiles />
+          </Route>
+          <Route path="/profile">
+            <Profile user={user} />
+          </Route>
+          <Route path="/404">
+            <div className="card col-lg-14">
+            <img className="rounded mx-auto d-block" src="https://thumbs.gfycat.com/EveryWealthyHatchetfish-small.gif" width="600" height="300" alt="" />
+            <h1 className="text-center">404</h1>
+            </div>
+          </Route>
+        </Router>
+      </div>
+      <div style={{ paddingTop: '80px' }}>
+        <Footer />
+      </div>
     </div>
   );
 }
