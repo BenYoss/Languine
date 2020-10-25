dfbc9283235530a9990cfdd10050c92cbd2d4a85
benyoss <benyoss4@gmail.com>
2020-10-24 22:52:23 -0500

    (styling) rearranged and polished the form

--- client/src/components/join.jsx
+++ client/src/components/join.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Collapse } from 'react-bootstrap';
+import { Collapse, Button } from 'react-bootstrap';
 import { getAccount, addPassword, hash } from '../helpers/helpers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -19,16 +19,16 @@ function Join() {
   }, []);
 
   return (
-    <div className="join-outer-container col-lg-8" style={{ marginLeft: '100px' }}>
+    <div className="card join-outer-container col-lg-13 pb-30" style={{ marginLeft: '100px' }}>
       <div className="join-inner-container">
-        <h1 className="d-flex join-header justify-content-center variant-dark">Create Room</h1>
+        <h1 className="card-header bg-dark text-white d-flex join-header justify-content-center variant-dark">Create Room</h1>
         <div>
           <label>Insert a name:</label>
           <input placeholder="Roomname" className="Room-Input form-control" type="text" onChange={(ev) => setRoom(ev.target.value)} />
           <label>Room Description:</label>
           <textarea placeholder="Description" className="Desc-Input form-control at-20" type="text" onChange={(ev) => setDesc(ev.target.value)} />
           <h4>Set public?</h4>
-          <button type="button" onClick={() => setPub(!pub)}>{`${pub}`}</button>
+          <Button type="Button" onClick={() => setPub(!pub)}>{`${pub}`}</Button>
           <div>
             <div>
               <Collapse className="pt-3 pb-4 col-lg-8" in={!pub}>
@@ -39,23 +39,24 @@ function Join() {
               </Collapse>
             </div>
           </div>
-          <button
-            className="d-flex"
-            type="submit"
-            onClick={() => {
-              if (pass) {
-                console.log('password');
-                addPassword(hash(pass), room).then((data) => {
-                  console.log(data);
-                  setTimeout(() => { window.location.href = `/discussion?name=${user.id_google}&room=${room}&user=${user.username}&desc=${desc}&pub=${pub}`; }, 1000);
-                });
-              } else {
-                window.location.href = `/discussion?name=${user.id_google}&room=${room}&user=${user.username}&desc=${desc}&pub=${pub}`;
-              }
-            }}
-          >
-            Create Room
-          </button>
+          <div className="d-flex join-header justify-content-center variant-dark">
+            <Button
+              type="submit"
+              onClick={() => {
+                if (pass) {
+                  console.log('password');
+                  addPassword(hash(pass), room).then((data) => {
+                    console.log(data);
+                    setTimeout(() => { window.location.href = `/discussion?name=${user.id_google}&room=${room}&user=${user.username}&desc=${desc}&pub=${pub}`; }, 1000);
+                  });
+                } else {
+                  window.location.href = `/discussion?name=${user.id_google}&room=${room}&user=${user.username}&desc=${desc}&pub=${pub}`;
+                }
+              }}
+            >
+              Create Room
+            </Button>
+          </div>
         </div>
       </div>
     </div>
